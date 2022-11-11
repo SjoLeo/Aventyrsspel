9aeaac431e149b3f143de1220cc51ed94f3a2ac5
SjoLeo <116351189+SjoLeo@users.noreply.github.com>
2022-11-11 14:25:21 +0100

    Update Monster.py

--- Monster.py
+++ Monster.py
@@ -1,8 +1,24 @@
+import random as rand
 class Monster():
-    def __init__(self, STR, HP):
+    def __init__(self, typ, STR, HP):
         self.STR = STR
         self.HP = HP
+        self.typ = typ
 
 
-Zombie = Monster(3, 5)
+    def typ(self):
 
+        monster = rand.randint(1, 10)
+
+        if monster in range(1, 5):
+            typ = "zombie"
+
+        else:
+            typ = "spindel"
+
+# def STR(self):
+
+
+# def HP(self):
+
+Zombie = Monster("zombie", 3, 5)
