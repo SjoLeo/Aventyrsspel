15a8ca927a8d139383b152ade60974a439043cdf
SjoLeo <116351189+SjoLeo@users.noreply.github.com>
2022-11-11 14:23:27 +0100

    Update and rename Main.py to Main_2.py

--- Main_2.py
+++ Main_2.py
@@ -1,13 +1,21 @@
 from Items import *
 from Monster import *
 from Player import *
+from main import *
 
+# weapon = 2 slot
+weapon = []
+
+# armour = 2 slot
+armour = []
+
+# potion = 2 slot
+potion = []
+
+# inventory = 6 slots
 Inventory = [LongSword, BroadSword, Axe, Chestplate, Potion_of_health]
 
-print(player.STR, player.HP, player.DEF)
 
-# Lägga till item stats på spelare
+# Add item stats to player
 for item in Inventory:
     player.add_stats(item.STR, item.HP, item.DEF)
-
-print(player.STR, player.HP, player.DEF)
