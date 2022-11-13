2adbc2a31f4517b35a62b8eeb95ce330499dd479
Leo.s <leo.sjostrom@skola.taby.se>
2022-11-13 20:06:46 +0100

    .

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ pygame.display.set_caption("Test Caption")
 # pygame.display.set_icon(icon)
 inventory_image = pygame.image.load('Images/Inventory_slot.png').convert_alpha()
 
-inventory_button = Buttons.Button(inventory_image, 0, 0, 0.4)
+inventory_button = Buttons.Button(inventory_image, 0, 0, 0.3)
 
 # Game Loop
 
 while running:
 
     screen.fill((60, 50, 217))
-    # checks if action happened
+    # checks if action happened and draws image
     if inventory_button.draw_button(screen):
         print('clicked')
 
