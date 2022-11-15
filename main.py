68b4c485c3386350f37e7a0d64367309778c7e31
Leo.s <leo.sjostrom@skola.taby.se>
2022-11-15 10:08:33 +0100

    exit button test

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ pygame.display.set_caption("Test Caption")
 # icon = pygame.image.load("")
 # pygame.display.set_icon(icon)
 inventory_image = pygame.image.load('Images/Inventory_slot.png').convert_alpha()
+exit_image = pygame.image.load('Images/exit.png').convert_alpha()
 
 inventory_button = Buttons.Button(inventory_image, 0, 0, 0.3)
-
+exit_button = Buttons.Button(exit_image, 1100, 0, 0.5)
 # Game Loop
 
 while running:
@@ -29,6 +30,9 @@ while running:
     # checks if action happened and draws image
     if inventory_button.draw_button(screen):
         print('clicked')
+
+    if exit_button.draw_button(screen):
+        running = False
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
