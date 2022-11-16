5a6faf9db5032fdceb68a8a9ffc3d780f275f8c8
Leo.s <leo.sjostrom@skola.taby.se>
2022-11-16 18:09:37 +0100

    lektion

--- Main_2.py
+++ Main_2.py
@@ -3,14 +3,7 @@ import Items
 import Player
 
 
-# Add item stats to player
-'''for item in Items.Inventory:
-    Player.player.add_stats(item.strength, item.hp, item.defence)
-print(Player.player.strength, Player.player.hp, Player.player.defence)'''
-# Add items to inventory
-print(len(Items.Inventory))
-Items.LongSword.add_item_to_inventory()
-print(len(Items.Inventory))
+
 
 
 
